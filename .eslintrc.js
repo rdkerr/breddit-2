f72a46a8acee4ab6ece0bd5d8297b50e4ee5c4b2
rdkerr <rogerdkerr@gmail.com>
2022-07-26 12:04:11 -0700

    refactor: extract config files

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true, // Browser global variables like `window` etc.
+    commonjs: true, // CommonJS global variables and CommonJS scoping.Allows require, exports and module.
+    es6: true, // Enable all ECMAScript 6 features except for modules.
+    jest: true, // Jest global variables like `it` etc.
+    node: true, // Defines things like process.env when generating through node
+  },
+  extends: [],
+  parser: "babel-eslint",
+  parserOptions: {
+    sourceType: "module",
+  },
+  plugins: [],
+};
